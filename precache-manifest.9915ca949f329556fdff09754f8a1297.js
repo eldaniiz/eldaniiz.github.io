c8469b9735a0ed36cb971b1dc8f729de8609ad39
Eldaniz Babayev <eldanizbabayev@protonmail.com>
2021-05-28 23:27:25 +0400

    Updates

---
+++ precache-manifest.9915ca949f329556fdff09754f8a1297.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333ab0af4d6ebd6ea2e31eecbc16b53b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4af6173b8d8e6daa852e",
+    "url": "/static/css/2.ffdccd6e.chunk.css"
+  },
+  {
+    "revision": "7056e0206d7a7263f9c6",
+    "url": "/static/css/main.2c1f0016.chunk.css"
+  },
+  {
+    "revision": "4af6173b8d8e6daa852e",
+    "url": "/static/js/2.6315858c.chunk.js"
+  },
+  {
+    "revision": "b3dbcb33eadca65d788274b2b68b263c",
+    "url": "/static/js/2.6315858c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7056e0206d7a7263f9c6",
+    "url": "/static/js/main.2ca5c1c1.chunk.js"
+  },
+  {
+    "revision": "9b6c1fe951deda90c22b",
+    "url": "/static/js/runtime-main.2ebb9a0d.js"
+  },
+  {
+    "revision": "80f97897a43d623b9a2ffafcab73e10c",
+    "url": "/static/media/me.80f97897.jpg"
+  }
+]);
